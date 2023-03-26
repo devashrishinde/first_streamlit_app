b91a4a5b5d475929a6b37d8bedb9535b9e9e73e6
devashrishinde <74109459+devashrishinde@users.noreply.github.com>
2023-03-25 23:37:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
-            streamlit.error('Please select a fruit tp get information.')
+            streamlit.error('Please select a fruit to get information.')
     else:    
             back_from_function = get_fruityvice_data(fruit_choice)
             streamlit.dataframe(back_from_function)
@@ -55,14 +55,14 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows=get_fruit_load_list()
-    my_cnx.close();
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 #Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
-    return "Thanks for adding" + new_fruit
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
+        return "Thanks for adding" + new_fruit
 
 # Take the json version of the response and normalize it 
 # output it the scsreen as a table
